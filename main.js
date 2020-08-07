0561b2d9db67d9b67880362c569b4f1f3c757ed5
John Doe <johndoe@MacBookPro.local>
2020-08-07 21:05:59 +0200

    New message div.

---
+++ main.js
@@ -0,0 +1,73 @@
+const btn = document.getElementById('emoji');
+const messageDiv = document.getElementById('message');
+const clearBtn = document.getElementById('clear');
+const showBtn = document.getElementById('show');
+const addBtn = document.getElementById('add');
+
+let message = ['Hi, I\'ll be your advisor.','I can decide for you.','Give me some options!', 'Click on me to decide']
+let options = [];
+let counter = 0;
+
+const clearMessage = () => {
+    messageDiv.textContent = '';
+}
+
+let clearOpts = () => {
+    options = [];
+    messageDiv.textContent = 'Options cleared!';
+    setTimeout(clearMessage, 5000);
+};
+
+let showOpts = () => {
+    if (options.length >= 1) {
+        messageDiv.textContent = `Options: ${[...options]}.`;
+    } else {
+        messageDiv.textContent = 'Sorry, no options.'
+    }
+    setTimeout(clearMessage, 2500);
+};
+
+let addOpts = (e) => {
+    let newOpt = document.querySelector('input').value;
+    if (newOpt) {
+        options.push(newOpt);
+        messageDiv.textContent = `Added new option!`;
+    } else {
+        messageDiv.textContent = `Please enter option value 🙄`;
+    }
+    
+    document.querySelector('input').value = '';
+    setTimeout(clearMessage, 2500);
+}
+
+clearBtn.addEventListener('click', clearOpts);
+showBtn.addEventListener('click', showOpts);
+addBtn.addEventListener('click', addOpts);
+btn.addEventListener('click', function() {
+    if(options.length) {
+        messageDiv.textContent = options[Math.floor(Math.random()*options.length)];
+    } else {
+        messageDiv.textContent = 'Sorry, no options.'
+    }
+    setTimeout(clearMessage, 5000);
+});
+
+document.querySelector('input').addEventListener('keyup', function(e) {
+    if(e.keyCode === 13) {
+        addOpts()
+    }
+})
+
+const init = () => {
+    for(let i = 0; i < 2; i++) {
+        messageDiv.textContent = message[i];
+        setTimeout(clearMessage, 2500);;
+    }
+    setTimeout(function() {
+        if(options.length == 0) {
+            messageDiv.textContent = message[2];
+            setTimeout(clearMessage, 5000);;
+        }
+    }, 10000)  
+}
+init();
