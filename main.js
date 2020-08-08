26a77ba05e2eda3d17b08530ec388a9b003b587d
John Doe <johndoe@MacBookPro.local>
2020-08-08 21:39:09 +0200

    New message bar
    git status

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ const messageDiv = document.getElementById('message');
 const clearBtn = document.getElementById('clear');
 const showBtn = document.getElementById('show');
 const addBtn = document.getElementById('add');
+const msgWrapper = [...document.getElementsByClassName('msg-wrapper')];
 
 let message = ['Hi, I\'ll be your advisor.','I can decide for you.','Give me some options!', 'Click on me to decide']
 let options = [];
 let counter = 0;
 
+const addShadow = () => {
+    msgWrapper[0].classList.add('add-shadow');
+}
+
 const clearMessage = () => {
     messageDiv.textContent = '';
+    msgWrapper[0].classList.remove('add-shadow');
 }
 
 let clearOpts = () => {
     options = [];
+    addShadow();
     messageDiv.textContent = 'Options cleared!';
     setTimeout(clearMessage, 5000);
 };
 
 let showOpts = () => {
+    addShadow();
     if (options.length >= 1) {
         messageDiv.textContent = `Options: ${[...options]}.`;
     } else {
@@ -29,13 +37,13 @@ let showOpts = () => {
 
 let addOpts = (e) => {
     let newOpt = document.querySelector('input').value;
+    addShadow();
     if (newOpt) {
         options.push(newOpt);
         messageDiv.textContent = `Added new option!`;
     } else {
         messageDiv.textContent = `Please enter option value 🙄`;
     }
-    
     document.querySelector('input').value = '';
     setTimeout(clearMessage, 2500);
 }
@@ -44,6 +52,7 @@ clearBtn.addEventListener('click', clearOpts);
 showBtn.addEventListener('click', showOpts);
 addBtn.addEventListener('click', addOpts);
 btn.addEventListener('click', function() {
+    addShadow();
     if(options.length) {
         messageDiv.textContent = options[Math.floor(Math.random()*options.length)];
     } else {
@@ -58,6 +67,7 @@ document.querySelector('input').addEventListener('keyup', function(e) {
     }
 })
 
+/*
 const init = () => {
     for(let i = 0; i < 2; i++) {
         messageDiv.textContent = message[i];
@@ -71,3 +81,4 @@ const init = () => {
     }, 10000)  
 }
 init();
+*/
